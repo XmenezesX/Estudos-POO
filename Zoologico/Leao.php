fd8929a182e46d0a50af15519ac3f793dc38f147
Yan Menezes <pignata.yan@gmail.com>
2024-01-15 19:31:40 -0300

    .

--- Zoologico/Leao.php
+++ Zoologico/Leao.php
@@ -2,6 +2,15 @@
 include 'Animal.php';
 
 class Leao extends Animal{
+    public function __construct()
+    {
+        $this->som = "miauuu";
+    }
+
+    public function setSomLeao($novoSom){
+        $this->som = $novoSom;
+    }
+
     public function ReproduzirSom(){
         return $this->som;
     }
--- Zoologico/Zebra.php
+++ Zoologico/Zebra.php
@@ -2,6 +2,15 @@
 include "Animal.php";
 
 class Zebra extends Animal{
+    public function __construct()
+    {
+        $this->som = "iiiinhrr";
+    }
+
+    public function setSomZebra($novoSom){
+        $this->som = $novoSom;
+    }
+
     public function ReproduzirSom(){
         return $this->som;
     }
